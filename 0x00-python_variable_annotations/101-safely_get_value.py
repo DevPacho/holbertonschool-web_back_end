d959edc767a5e04f294fe93c511d43e85c672b4a
DevPacho <frankjose2004@gmail.com>
2023-02-17 00:59:24 -0500

    Defining function that works with any type, but still allows you to specify the relationship between the types of the function's arguments

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""More involved type annotations"""
+
+from typing import Mapping, Any, Union, TypeVar
+
+
+def safely_get_value(dct: Mapping, key: Any,
+        default: Union[TypeVar("T"), None] = None) -> Union[Any, TypeVar("T")]:
+    """
+    Returns the value associated with the key in the mapping,
+    or None as default if the key is not found.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
